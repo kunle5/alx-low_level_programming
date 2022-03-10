d5c8b59c24c5441b2b684fc44b2ac8fec33a01d3
kunle5 <abdulhafizaderemi@gmail.com>
2022-03-10 12:39:56 -0800

    kunle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#imclude <stdio.h>
+
+/**
+* main - prints the sentence "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+A repo for the first project of C programming
