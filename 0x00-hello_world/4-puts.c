0f3383693b3148e9d6ea49de179610209a4e24dd
kunle5 <abdulhafizaderemi@gmail.com>
2022-03-10 13:24:25 -0800

    kunle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - prints the phrase "Programming is like building a multilingual puzzle
+* main - prints the sentence - Programming is like building a m multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
