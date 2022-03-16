c0205e7430403779192280324ea4ccf6498760c8
kunle5 <abdulhafizaderemi@gmail.com>
2022-03-16 12:12:07 -0700

    kunle

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds 2 int
+ * and return the result
+ * @a: a is an int
+ * @b: b is an int
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
+
