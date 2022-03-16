53e0a92ed792a2a92f8644218758a39b45416187
kunle5 <abdulhafizaderemi@gmail.com>
2022-03-16 07:35:23 -0700

    kunle

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _abs - prints absolute value
+ * @n: n is an integer
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	if(n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
