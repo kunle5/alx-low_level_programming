db27dc0316627c56357071737923e6f9e015be3c
kunle5 <abdulhafizaderemi@gmail.com>
2022-03-16 07:36:45 -0700

    kunle

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * int _abs - prints absolute value
+ * _abs - prints absolute value
  * @n: n is an integer
  * Return: int
  */
 
 int _abs(int n)
 {
-	if(n < 0)
+	if (n < 0)
 	{
 		return (-n);
 	}
