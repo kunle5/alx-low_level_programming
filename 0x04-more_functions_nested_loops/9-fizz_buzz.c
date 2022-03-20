e2e3ec2b6b75ef6122118531f41210e49f9c6db9
kunle5 <abdulhafizaderemi@gmail.com>
2022-03-20 14:40:23 -0700

    kunle

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i = 1;
 
-	for (; i <= 100; i++)
+	for (; i < 100; i++)
 	{
-		if (i % 15)
+		if (i % 15 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
