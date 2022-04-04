6667a7d1d6715a602cbfcba0b15f02ceadf802db
kunle5 <abdulhafizaderemi@gmail.com>
2022-04-04 13:49:59 -0700

    0-isupper completed

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks uppercase
+ * @c: variable
+ * Return: 1 if uppercase,
+ * 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
