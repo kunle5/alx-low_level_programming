5a30846e9b3192f93a357710c4243584c1f69881
kunle5 <abdulhafizaderemi@gmail.com>
2022-04-06 14:39:46 -0700

    kunle

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - fress a 2D grid
+ * @grid: multi-dimensional array of intehers
+ * @height: height of the grid
+ * Return: no return
+ */
+
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (i = 0; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
