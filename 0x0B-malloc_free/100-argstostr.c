8257b957c06bf3a56c63586c749b984792604a53
kunle5 <abdulhafizaderemi@gmail.com>
2022-04-06 21:39:48 -0700

    finally!

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,14 +13,15 @@ char *argstostr(int ac, char **av)
 	int i, j, k, len;
 	char *str;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			len++;
 		len++;
 	}
-	str = (char *) malloc(sizeof(char) * (len + 1));
-	k = 0;
+	str = malloc(sizeof(char) * len);
 	if (str == NULL)
 	{
 		return (NULL);
@@ -33,8 +34,8 @@ char *argstostr(int ac, char **av)
 			str[k] = av[i][j];
 			k++;
 		}
-	str[k] = '\n';
-	k++;
+		str[k] = '\n';
+		k++;
 	}
 	return (str);
 }
